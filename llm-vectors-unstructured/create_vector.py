33a7a0ebdd733a7b21d0ca9f66ee0ddbf022fa35
Yari Tkachuk <coderpyt@gmail.com>
2025-09-10 14:43:43 +0200

    feat: chunk, embed, and import data to Neo4j graph DB

--- llm-vectors-unstructured/create_vector.py
+++ llm-vectors-unstructured/create_vector.py
@@ -1,20 +1,41 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_neo4j import Neo4jVector
+from langchain_openai import OpenAIEmbeddings
+
+
 load_dotenv()
 
-from langchain_community.document_loaders import DirectoryLoader, TextLoader
 
-COURSES_PATH = "llm-vectors-unstructured/data/asciidoc"
+COURSES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/asciidoc")
 
 # Load lesson documents
 loader = DirectoryLoader(COURSES_PATH, glob="**/lesson.adoc", loader_cls=TextLoader)
 docs = loader.load()
 
 # Create a text splitter
-# text_splitter =
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=1500,
+    chunk_overlap=200,
+)
 
 # Split documents into chunks
-# chunks =
+chunks = text_splitter.split_documents(docs)
+print(len(chunks))
 
 # Create a Neo4j vector store
-# neo4j_db =
+neo4j_db = Neo4jVector.from_documents(
+    chunks,
+    OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY")),
+    url=os.getenv("NEO4J_URI"),
+    username=os.getenv("NEO4J_USERNAME"),
+    password=os.getenv("NEO4J_PASSWORD"),
+    database="neo4j",  
+    index_name="chunkVector",
+    node_label="Chunk", 
+    text_node_property="text", 
+    embedding_node_property="embedding",  
+)
