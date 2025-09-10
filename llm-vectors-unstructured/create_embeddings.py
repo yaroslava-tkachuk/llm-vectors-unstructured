e79ff96a1a0494410d62f65486bb27e0bd96078f
Yari Tkachuk <coderpyt@gmail.com>
2025-09-10 14:59:28 +0200

    feat: query Neo4j DB using vector similarity

--- llm-vectors-unstructured/create_embeddings.py
+++ llm-vectors-unstructured/create_embeddings.py
@@ -9,8 +9,8 @@ load_dotenv()
 llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 response = llm.embeddings.create(
-        input="Text to create embeddings for",
-        model="text-embedding-ada-002"
-    )
+    input="Text to create embeddings for",
+    model="text-embedding-ada-002"
+)
 
 print(response.data[0].embedding)
--- llm-vectors-unstructured/query_neo4j.py
+++ llm-vectors-unstructured/query_neo4j.py
@@ -1,23 +1,35 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
+from langchain_neo4j import Neo4jGraph
+
+
 load_dotenv()
 
-from openai import OpenAI
 
-llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 response = llm.embeddings.create(
-        input="What does Hallucination mean?",
-        model="text-embedding-ada-002"
-    )
+    input="What does Hallucination mean?",
+    model="text-embedding-ada-002"
+)
 
 embedding = response.data[0].embedding
 
 # Connect to Neo4j
-# graph = 
+graph = Neo4jGraph(
+    url=os.getenv("NEO4J_URI"),
+    username=os.getenv("NEO4J_USERNAME"),
+    password=os.getenv("NEO4J_PASSWORD")
+)
 
 # Run query
-# result = 
+result = graph.query("""
+CALL db.index.vector.queryNodes('chunkVector', 6, $embedding)
+YIELD node, score
+RETURN node.text, score
+""", {"embedding": embedding})
 
 # Display results
-# for row ... 
+for row in result:
+    print(row["node.text"], row["score"])
